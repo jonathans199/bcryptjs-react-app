a896c00fd11a9172b27515abaab8c4f29bec8fd3
jonathans199 <jso1919@gmail.com>
2021-06-16 18:37:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react'
+import './App.css'
+
+import bcrypt from 'bcryptjs'
+
+// SALT should be created ONE TIME upon sign up
+const salt = bcrypt.genSaltSync(10)
+// example =>  $2a$10$CwTycUXWue0Thq9StjUM0u => to be added always to the password hash
 
 function App() {
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+
+  function handleLoginForm() {
+    const email = emailInputRef.current.value
+    const password = passwordInputRef.current.value
+    const hashedPassword = bcrypt.hashSync(password, '$2a$10$CwTycUXWue0Thq9StjUM0u') // hash created previously created upon sign up
+
+    fetch('https://api.sampleapis.com/beers/ale', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: hashedPassword,
+      }),
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <form>
+          <input style={{ padding: '15px', borderRadius: '10px', margin: '10px' }} ref={emailInputRef} type='email' placeholder='Email' />
+          <input style={{ padding: '15px', borderRadius: '10px', margin: '10px' }} ref={passwordInputRef} type='password' placeholder='Password' />
+          <button
+            type='submit'
+            style={{ padding: '15px', borderRadius: '10px', margin: '10px' }}
+            onClick={e => {
+              e.preventDefault()
+              handleLoginForm()
+            }}>
+            Log In
+          </button>
+        </form>
+        <span>Your new SALT: {salt}</span>
+        <br />
+        <span>
+          Save this Salt, UPON sign up <br /> if you refresh it will generate a new SALT!!!
+        </span>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
